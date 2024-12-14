84ee3bf33522fc3bfc86e50db4a1ef0754cfcc6a
mmxfxqr <holodov.sas@yandex.ru>
2024-12-14 15:00:53 +0300

    add schema and struct of auth

---
+++ shared/components/modals/auth/forms/schemas.tsx
@@ -0,0 +1,20 @@
+import { z } from "zod";
+export const passwordSchema = z
+  .string()
+  .min(6, { message: "Пароль должен быть не менее 6 символов!" });
+export const formLoginSchema = z.object({
+  email: z.string().email({ message: "Введите корректную почту!" }),
+  password: passwordSchema,
+});
+export const formRegisterSchema = formLoginSchema
+  .merge(
+    z.object({
+      confirmPassword: passwordSchema,
+    })
+  )
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Пароли не совпадают!",
+    path: ["confirmPassword"],
+  });
+export type FormLoginSchema = z.infer<typeof formLoginSchema>;
+export type FormRegisterSchema = z.infer<typeof formRegisterSchema>;
