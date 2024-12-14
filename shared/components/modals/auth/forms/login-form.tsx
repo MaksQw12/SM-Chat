92e574ce420b642387672e9dcae94c0a9a92525e
admin <Maksim7142@yandex.ru>
2024-12-14 22:03:47 +0300

    ..

--- shared/components/modals/auth/forms/login-form.tsx
+++ shared/components/modals/auth/forms/login-form.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { formLoginSchema, FormLoginSchema } from "./schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Title } from "@/shared/components/title";
-import { FormInput } from "@/shared/components/form-input";
-import { Button } from "@/shared/components/ui/button";
-import { loginUser } from "@/shared/services/user";
-import toast from "react-hot-toast";
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { formLoginSchema, FormLoginSchema } from './schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Title } from '@/shared/components/title';
+import { FormInput } from '@/shared/components/form-input';
+import { Button } from '@/shared/components/ui/button';
+import { loginUser } from '@/shared/services/user';
+import toast from 'react-hot-toast';
 
 interface Props {
   onClose: () => void;
@@ -16,32 +16,27 @@ export const LoginForm: React.FC<Props> = ({ onClose }) => {
   const form = useForm<FormLoginSchema>({
     resolver: zodResolver(formLoginSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
   });
   const onSubmit = async (data: FormLoginSchema) => {
     try {
       const resp = await loginUser(data.email, data.password);
-      if(resp.status !== 200) throw Error();
+      if (resp.status !== 200) throw Error();
       onClose?.();
-      toast.success("Вы успешно вошли в аккаунт" , {icon: "👋"});
+      toast.success('Вы успешно вошли в аккаунт', { icon: '👋' });
     } catch (error) {
-        console.log("Error [LOGIN]: ", error)
-      toast.error("Неправильная почта или пароль" , {icon: "❌"});
+      console.log('Error [LOGIN]: ', error);
+      toast.error('Неправильный логин или пароль', { icon: '❌' });
     }
   };
   return (
     <FormProvider {...form}>
-      <form
-        className="flex flex-col gap-5"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
+      <form className="flex flex-col gap-5" onSubmit={form.handleSubmit(onSubmit)}>
         <div className="flex justify-between items-center">
           <Title text="Вход в аккаунт" size="md" className="font-bold" />
-          <p className="text-gray-400">
-            Введите свою почту, чтобы войти в свой аккаунт{" "}
-          </p>
+          <p className="text-gray-400">Введите свою почту, чтобы войти в свой аккаунт </p>
         </div>
         <FormInput name="email" label="E-mail" required />
         <FormInput name="password" label="Пароль" type="password" required />
